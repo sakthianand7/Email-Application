d8b0a4e6bcae83de5c91cb1c1acab5e6cd119425
sakthianand7 <sakthipraba1423@gmail.com>
2021-05-30 18:30:45 +0530

    Project Outline

---
+++ src/email/Email.java
@@ -0,0 +1,5 @@
+package src.email;
+
+public class Email {
+    
+}
---
+++ src/email/EmailApplication.java
@@ -0,0 +1,7 @@
+package src.email;
+
+public class EmailApplication {
+    public static void main(String[] args) {
+        
+    }
+}
