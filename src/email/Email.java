aab5eeac8dd564d104ff10a9cc3bd7a49686eacf
sakthianand7 <sakthipraba1423@gmail.com>
2021-05-30 18:48:37 +0530

    added method to get the department

--- src/email/Email.java
+++ src/email/Email.java
@@ -1,5 +1,35 @@
 package src.email;
-
+import java.util.Scanner;
 public class Email {
+    private int mailBoxCapacity;
+    private String firstName;
+    private String lastName;
+    private String passWord;
+    private String dept;
+    private String altEmail;
+
+    //constructor to receive first and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created:" + this.firstName + " " + this.lastName);
+        this.dept = getDept();
+        System.out.println(this.dept);
+    }
+
+    //Ask for department
+    private String getDept(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Department code : \n1 for Development \n2 for QA \n3 for Production \n4 for none \nEnter your choice ");
+        int deptChoice = sc.nextInt();
+        if(deptChoice ==1){return "dev";} else if(deptChoice==2){return "qa";}else if(deptChoice==3){return "prod";} else {return "";}
+    }
+    //generate password
+
+    //set mailbox capacity
+
+    //set alternate email id
+
+    //change the password
     
 }
--- src/email/EmailApplication.java
+++ src/email/EmailApplication.java
@@ -2,6 +2,6 @@ package src.email;
 
 public class EmailApplication {
     public static void main(String[] args) {
-        
+        Email obj = new Email("Sakthi","Anand");
     }
 }
